f38a0d3666cdea6f37a7df7460e8219a494f44ce
ivanearisty <ivanearisty@gmail.com>
2023-11-07 12:37:09 -0500

    setting up backend dockerfile, changed model path

--- backend/src/modules/parameters.py
+++ backend/src/modules/parameters.py
@@ -1,9 +1,11 @@
+import os
+
 class Parameters:
     mongodb_user = "main-user"
     mongodb_password = "e4yX8152tnKg"
     mongodb_host = "localhost" 
     mongodb_port = 27017
     mongodb_database = "mydatabase"
-    modelLocation = "backend/mlmodel/model-best"
+    modelLocation = os.path.abspath("backend/mlmodel/model-best") ##made absolute path so docker can access model
 
 parameters = Parameters()

5c9ffa90fc1e030bb9785ef349d6cf037b54c1e2
ivanearisty <ivanearisty@gmail.com>
2023-11-08 21:30:35 -0500

    deleted frontend to rebuild with angular 17

--- frontend/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    websocket_endpoint: "ws://localhost:8000/websocket"
-    // websocket_endpoint: "ws://host.docker.internal:8000/websocket"
-  };
--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    websocket_endpoint: "ws://localhost:8000/websocket"
-    // websocket_endpoint: "ws://host.docker.internal:8000/websocket"
-  };
